0c51c8756e20ac5f28813ffc2e76bdd7f3d78151
JuanCorrales180 <36160636+JuanCorrales180@users.noreply.github.com>
2018-05-18 12:03:33 -0500

    Add Splash

--- Barbershop/Barbershop.Android/MainActivity.cs
+++ Barbershop/Barbershop.Android/MainActivity.cs
@@ -1,15 +1,17 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
+﻿
 namespace Barbershop.Droid
 {
-    [Activity(Label = "Barbershop", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    using Android.App;
+    using Android.Content.PM;
+    using Android.OS;
+
+    [Activity(
+        Label = "Barbershop", 
+        Icon = "@drawable/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ Barbershop/Barbershop.Android/SplashActivity.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+namespace Barbershop.Droid
+{
+    using Android.App;
+
+    using Android.OS;
+
+    [Activity(
+        Theme = "@style/Theme.Splash",//indicative the themes
+        MainLauncher = true, //set it as boot activity
+        NoHistory = true)] // 
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your application here
+            System.Threading.Thread.Sleep(1800);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
